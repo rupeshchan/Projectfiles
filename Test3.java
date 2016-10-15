bb0f8a44841d64e522223a0d3e32edf66d487b06
rupeshchan <rupeshchan@gmail.com>
2016-10-15 07:22:15 +0400

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,104 @@
+package com;
+
+import java.util.Scanner;
+
+public class Test3 {
+	int a=0;
+	int b=0;
+	int c=0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			
+		Test3 t3 = new Test3();
+		t3.input();
+	    t3.greater();
+		t3.lesser();
+		new Test3().swap();		
+		
+	}
+	
+	public void input(){
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter First number:");
+		if(reader.hasNextInt()){
+			a=reader.nextInt();	
+		}
+		
+		System.out.println("Enter second number:");
+		if(reader.hasNextInt()){
+			b=reader.nextInt();	
+		}
+		
+		System.out.println("Enter second number:");
+		if(reader.hasNextInt()){
+			c=reader.nextInt();	
+		}
+	
+		reader.close();
+	}
+	
+	
+	public void greater(){
+		
+		if((a>b) && (a>c)){
+			
+			System.out.println(a + " " + "A is Greater");
+			
+		}else if((b>a) && (b>c)){
+			
+			System.out.println(b + " " + "B is Greater");
+		}else if((c>a) && (c>b)){
+			
+			System.out.println(c + " " + "C is Greater");
+			
+		}
+		
+		
+			
+		}
+		
+	
+public void lesser(){
+	if((a<b) && (a<c)){
+		
+		System.out.println(a + " " + "A is Lesser");
+		
+	}else if((b<a) && (b<c)){
+		
+		System.out.println(b + " " + "B is Lesser");
+	}else if((c<a) && (c<b)){
+		
+		System.out.println(c + " " + "C is Lesser");
+		
+	}
+	
+		}
+		
+	
+
+public void swap(){
+	
+	int x = 1001;
+	int y = 25;
+	
+	x = x+y;
+	y=x-y;
+	x=x-y;
+	
+	System.out.println(x + " " + "X value");
+	System.out.println(y+" "+"Y value");
+	
+	
+	
+}
+
+
+
+	}
+	
+		
+	
+	
